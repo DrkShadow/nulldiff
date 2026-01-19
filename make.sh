5aff1b4a3cff7647f47e9f4978bf4a1dd346d8ba
DrkShadow <git@drkshadow>
2026-01-19 07:08:25 -0500

    Correct make file

--- make.sh
+++ make.sh
@@ -2,11 +2,11 @@
 
 opt=()
 if [ "$1" = "-o" ]; then
-	opt=( "-O2" -march=native -mtune=native)
+	opt=( "-O2" )
 fi
 
 #gcc -std=c23 -ggdb3 -o nullcombine  nullcombine.c
-gcc "${opt[@]}" -std=c23 -ggdb3 -march=native -mtune=native -o nulldiff  nulldiff.c
-gcc "${opt[@]}" -std=c23 -ggdb3 -march=native -mtune=native -o hashole  hashole.c
-gcc "${opt[@]}" -std=c23 -ggdb3 -march=native -mtune=native -o hasnull  hasnull.c
+gcc "${opt[@]}" -std=c23 -ggdb3 -march=native -o nulldiff  nulldiff.c
+gcc "${opt[@]}" -std=c23 -ggdb3 -march=native -o hashole  hashole.c
+gcc "${opt[@]}" -std=c23 -ggdb3 -march=native -o hasnull  hasnull.c
 
